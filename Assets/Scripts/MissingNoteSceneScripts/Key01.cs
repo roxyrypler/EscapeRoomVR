04ac4de76e0ecc0d427d6549408ecee2693370bf
roxyrypler <30288569+roxyrypler@users.noreply.github.com>
2019-07-16 20:10:29 +0200

    Added key 01 and door one logic, added books with rigid body

---
+++ Assets/Scripts/MissingNoteSceneScripts/Key01.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key01 : MonoBehaviour
+{
+    // detect if key intercects keyhole 01
+
+    public GameObject doorKeyOne;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "KeyHole01")
+        {
+            doorKeyOne.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+}
